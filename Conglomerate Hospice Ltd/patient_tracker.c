83b6c0116594498d8b01f3ec5886cd59f69ecaa8
Shemar Jones <jones.shemar.sj@gmail.com>
2021-03-13 12:37:08 -0500

    Added the Psuedocode
    More tweaks to the Menu in the main function. Added the function body for the other declared functions, along with comments.

--- Conglomerate Hospice Ltd/patient_tracker.c
+++ Conglomerate Hospice Ltd/patient_tracker.c
@@ -16,11 +16,8 @@ void Exit_WithSave();
 void Exit_WithoutSave();
 
 // Global Constants
-// int PSIZE = 35;
-// int RSIZE = 21;
-// int MAX_BEDS = 5;
-#define PSIZE 35
-#define RSIZE 21
+int PSIZE = 35;
+int RSIZE = 21;
 #define MAX_BEDS 5
 
 // Structures - Patient and Room
@@ -48,23 +45,34 @@ typedef struct Room
 int main()
 {
     // Variable declarations & Allocating Memory
-    int user_option;
+    int choice = 0;
 
-    // Displays the Menu
-    printf("\n*******************************");
-    printf("\n*  Conglomerate Hospice Ltd.  *");
-    printf("\n*******************************");
-    printf("\n\'1\' - Add A Patient.");
-    printf("\n\'2\' - Add A Room.");
-    printf("\n\'3\' - Change Patient Status.");
-    printf("\n\'4\' - AI Assignment.");
-    printf("\n\'0\' - Exit.");
-    printf("\n*******************************");
-    printf("\nEnter Option here: ");
-    scanf("%d", &user_option); // Accepts the Users-Option
+    while ((choice != 9) || (choice != 10))
+    {
+        // Displays the Menu
+        printf("\n==============================================");
+        printf("\nConglomerate Hospice Ltd");
+        printf("\nCOVID-19 Patients tracker");
+        printf("\n==============================================");
+        printf("\nMENU");
+        printf("\n------------------------------------------------------------");
+        printf("\n\t1  - Add A Patient ");
+        printf("\n\t2  - Add A Room");
+        printf("\n\t3  - Change Patient Status");
+        printf("\n\t4  - AI Assignment");
+        printf("\n\t5  - Discharge");
+        printf("\n\t6  - Room admittance by age group Report");
+        printf("\n\t7  - Admitted patient list Report");
+        printf("\n\t8  - COVID-19 status Report");
+        printf("\n\t9  - Exit and Save");
+        printf("\n\t10 - Exit without save");
+        printf("\n------------------------------------------------------------");
+        printf("\nEnter Your Choice: ");
+        scanf("%d", &choice); // Accepts the Users-Option
+    }
 
     // Implement switch function
-    switch (user_option)
+    switch (choice)
     {
     case 1:
         AddPatient(); // Leads to function
@@ -82,11 +90,27 @@ int main()
         AI_Assignment();
         break;
 
-    case 0:
-        system("cls"); // Clears the screen
-        printf("** Goodbye **");
-        exit(0); // Ends the Program
+    case 5:
+        Discharge();
         break;
+
+    case 6:
+        RoomReport();
+        break;
+
+    case 7:
+        AdmittedReport();
+        break;
+
+    case 8:
+        StatusReport();
+        break;
+
+    case 9:
+        Exit_WithSave();
+
+    case 10:
+        Exit_WithoutSave();
 
     default:
         system("cls"); // Clears the screen
@@ -108,8 +132,6 @@ that new entries follow the field requirements
 */
 void AddRoom()
 {
-
-    int Password;
     system("cls"); //clears screen
 }
 
@@ -133,8 +155,54 @@ void ChangeStatus()
 
 /*
 Allows the system to assign those patients who do not yet have a room based on
-the following protocols:
+the following protocols:.........
+Note that the order of the priority depends on the top-down design of the table whereby those
+listed first get priority over the others.
 */
 void AI_Assignment()
 {
 }
+
+/*
+The patient’s record is changed from admitted to discharged and the patient is
+removed from the room they were assigned (if they were assigned a room). Patients are
+discharged based on their unique id numbers.
+*/
+void Discharge() {}
+
+/*
+Room admittance by age group Report: A bar chart frequency report that will show the number
+of persons currently assigned to rooms grouped by their age groups: over 60s, 40 to 60, 20 to 39
+and younger than 39. The user should then be given the option to save the report to a file (the
+system will request a file name from the user to save the file).
+*/
+void RoomReport() {}
+
+/*
+Admitted patient list Report: A list of all of the patients currently admitted ordered by their id
+numbers (in ascending order) showing their id numbers, names, room they are currently
+assigned (“none” if not yet assigned a room) and current COVID-19 status. The user should then
+be given the option to save the report to a file (the system will request a file name from the user
+to save the file).
+*/
+void AdmittedReport() {}
+
+/*
+COVID-19 status Report: bar chart frequency report that will show the number of persons
+grouped by each status: none, mild, average, severe and critical. The user should then be given
+the option to save the report to a file, which file name they supply to the system. This report
+should be ordered by frequency from highest to lowest (descending order). The user should
+then be given the option to save the report to a file (the system will request a file name from
+the user to save the file).
+*/
+void StatusReport() {}
+
+/*
+Exit: close the system with automatic save of changes made to the file.
+*/
+void Exit_WithSave() {}
+
+/*
+Exit without save: abruptly close the system disregarding all changes made to the system.
+*/
+void Exit_WithoutSave() {}
